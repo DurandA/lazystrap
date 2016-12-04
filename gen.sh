4879c9b0969f1aa802c9341b158a8e15037b949e
Arnaud Durand <arnaud.durand@live.com>
2016-12-04 19:36:50 +0100

    Generate logo using ImageMagick

---
+++ gen.sh
@@ -0,0 +1,3 @@
+convert -size 383x48 xc:Transparent -gravity Center -weight 700 -pointsize 40 -fill '#F6F6EB' -font Red\ October.ttf -annotate 0 "redcorner.io" static/img/logo.png
+convert static/img/fist-158024.png -thumbnail 16x16 -alpha on -background none -flatten static/favicon.png
+convert static/favicon.png -define icon:auto-resize=64,48,32,16 static/favicon.ico
